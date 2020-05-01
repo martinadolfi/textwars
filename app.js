1096585945b3c25900b867831caa56b00c74dc9e
Martin Adolfi <martinadolfi@gmail.com>
2020-05-01 17:48:10 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+startVelocity=3000;
+velocity = startVelocity;
+startWordInterval = 2000;
+wordInterval = startWordInterval;
+moveDistance = 30;
+moveDistancePx="+=" + moveDistance + "px";
+verticalLimit = 600;
+hLimit = 600;
+finishWords = [13,32]
+wordCount = wordList.length
+currentScore = 0;
+running = true;
+var myTimer,wordTimer
+
+function moveDown(){
+    $( ".word" ).animate({'top': moveDistancePx});
+    checkPosition();
+}
+
+function checkPosition(){
+    $( ".word" ).each( function( index, element ){
+        if ( parseInt($( this ).css('top').replace("px")) > verticalLimit ) {
+            running = false;
+        }
+    });
+}
+
+function getScoreFromWord(word){
+    return word.length;
+};
+
+function keyPressed(box,event){
+    if ( finishWords.includes(event.which) )  {
+        currentWord=box.val()
+        $( ".word" ).each(function (index,element){
+            if ( element.innerText == currentWord ) {
+                element.remove();
+                currentScore+=getScoreFromWord(currentWord)
+                $( "#currentScore" ).text(currentScore);
+            }});
+        box.val('');
+    } else {
+        currentWord=box.val() + String.fromCharCode(event.which);
+    }
+}
+function createOneWord(){
+    jQuery('<div class="word" style="left:'+ Math.round(Math.random()*hLimit)+'px">'+ wordList[Math.round(Math.random()*wordCount)]+'</div>').appendTo('#gameScreen');
+}
+
+function ramper(){
+    if (running) {
+        myTimer = setTimeout(function(){
+            moveDown();
+            ramper();
+        },startVelocity-(currentScore*10));
+    }
+}
+
+function creator(){
+    if (running) {
+        wordTimer = setTimeout(function(){
+            createOneWord();
+            creator();
+        },startWordInterval-(currentScore*5));
+    }
+}
+
+$(document).ready(function(){
+    // myTimer = setInterval(function(){
+    //       moveDown();    
+    // },velocity);
+    //wordTimer = setInterval(createOneWord,wordInterval);
+    $( "#myInput" ).keypress(function(event){
+        if ( finishWords.includes(event.which) ) {
+            event.preventDefault();
+         }       
+        keyPressed ($(this),event );
+    })
+    ramper();
+    creator();
+});
+
